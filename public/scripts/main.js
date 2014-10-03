04b015276c38d67e6b3d741a98b6d3d3167ae92f
Matthew Falbo <matthew.falbo@gmail.com>
2014-10-02 21:01:34 -0600

    Initial layout.  Planning elements and user interaction.  Need to link jQuery and make decisions about major frameworks and/or libraries.

---
+++ public/scripts/main.js
@@ -0,0 +1,11 @@
+var World = {
+	name: String,
+	player: Object,
+	rooms: Array,
+	items: Array,
+	Monsters: Array,
+	_createWorld: function(worldName, playerName){
+		//Instantiate a world object, a player object, world is created of an array of room objects, each room has monsters and/or objects
+	}
+}
+
